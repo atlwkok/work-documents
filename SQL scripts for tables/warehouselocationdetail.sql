a193e0cad2b23b0173f977cba8ae19ae612d9f15
atlwkok <weijin.kok@rndc-usa.com>
2026-04-21 08:13:03 -0400

    matm-spo-import: complete

--- SQL scripts for tables/warehouselocationdetail.sql
+++ SQL scripts for tables/warehouselocationdetail.sql
@@ -1,3 +1,4 @@
+ 
 
 -- do $$
 -- BEGIN
@@ -5,18 +6,17 @@
 -- 	END IF;
 
 -- insert enabled alerts without payload
---  db-alert-shipconf-24h-fail.
+--  db-alert-matm-spo-import.
 
 
 insert into app.warehouselocationdetail(description, longdescription, warehouseid, requesttypeid, createdby, lastupdatedby) 
     select rt.description, rt.description, w.warehouseid,  rt.requesttypeid, 'sysuser', 'sysuser' 
         from app.warehouselocation w, app.requesttype rt 
         where w.warehouseshortname not in ('DEFAULTS', 'PRODEV') 
-        and rt.description = 'db-alert-shipconf-24h-fail' 
+        and rt.description = 'db-alert-matm-spo-import' 
         and not exists (select 1 from app.warehouselocationdetail r 
                             where r.requesttypeid = rt.requesttypeid 
                             and r.warehouseid = w.warehouseid);
-
 
 
 
@@ -33,7 +33,7 @@ from app.warehouselocationdetail r, app.warehouselo
 
 select *
 	from app.warehouselocationdetail wld, app.warehouselocation wl
-	where wld.description like ('%db-alert-shipconf-24h-fail')
+	where wld.description like ('%db-alert-matm-spo-import')
 	AND wld.warehouseid = wl.warehouseid 
 	order by wl.warehouseshortname
 
@@ -44,11 +44,32 @@ select *
 
 -- insert payload for enabled alerts:
 
- 
+-- Named queries (enabled only (RNDC) as of writing)
+update app.warehouselocationdetail set payload = '{
+    "alertName":"MATM - SPO Import Failure",
+    "dataSource":"MAWM",
+    "database":{
+        "connection":"MAPRD_NEW",
+        "type":"MySQL",
+        "uri":"jdbc:{host}:{port}/information_schema"
+    },
+    "email":{
+        "subject":"Splunk Alert: $name$",
+        "body":"<pre>The alert condition for ''$name$'' was triggered.<br /><br />This alert highlights PO import errors in TMS. POs will not tender to a carrier until the data condition mentioned in the alert is corrected and the PO successfully imports into TMS.</pre>",
+        "fromEmail":"wms-tools-noreply@rndc-usa.com",
+        "toEmail":"IBTransportation@rndc-usa.com, InventoryPlanners-SupplyChain@rndc-usa.com, newsupplier@rndc-usa.com",
+        "ccEmail":"TMSTeam@RNDC-USA.COM, Tonja.Speegle@RNDC-USA.COM, Brenda.Weeks@RNDC-USA.COM, Bobby.Grant@RNDC-USA.COM, WMSMSPSupport@rndc-usa.com, Helena.Yates@rndc-usa.com, pam.thomas@rndc-usa.com, wmsinternal@rndc-usa.com",
+        "footer":"",
+        "attachment":["link-to-alert","link-to-results", "inline-table"]
+    },
+    "sql":"WITH t AS (SELECT CFM.created_timestamp, Substring_index(CFM.payload ->> ''$.PurchaseRequestId'', ''_'', 1) AS ''MARKET'', Substring_index(CFM.payload ->> ''$.PurchaseRequestId'', ''_'', -1) AS ''PO_NUMBER'', CFM.payload ->> ''$.BillingMethodId'' AS ''BILLING_METHOD'', CFM.payload ->> ''$.Extended.Buyer'' AS ''BUYER'', CFM.payload ->> ''$.Extended.SupplierCode'' AS ''SUPPLIER_CODE'', CFM.payload ->> ''$.Extended.SupplierName'' AS ''SUPPLIER_NAME'', CFM.error_message AS ''ERROR_CODE'', CFM.exception_messages ->> ''$[0].Description'' AS ''DESCRIPTION'' FROM default_commonutil.CUT_FAILED_MESSAGE CFM WHERE org_id = ''RNDC'' AND queue_name = ''XNT_VEN_PurchaseRequest'' AND error_code = ''VCO::171'' AND created_timestamp >= Now() - interval 1 day) SELECT * FROM t WHERE t.billing_method = ''Collect'' ORDER BY market;"
+}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='RNDC') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-matm-spo-import');
 
 
 
- -- (Unnamed)
+
+-- (Unnamed)
+-- unused as only 1 query enabled for RNDC orgId at this time and nothing else.
  
 
 
@@ -58,7 +79,7 @@ select *
 select wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description != 'db-alert-shipconf-24h-fail'
+where description != 'db-alert-matm-spo-import'
 and wld.payload is not null
 -- order by wld.payload
 
@@ -68,7 +89,7 @@ and wld.payload is not null
 select  wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description = 'db-alert-shipconf-24h-fail'
+where description = 'db-alert-matm-spo-import'
 and wld.payload is not null
 order by wld.lastupdatedon
 
@@ -82,6 +103,6 @@ FROM (
         wd.*, 
         row_number() OVER () AS rn
     FROM app.warehouselocationdetail wd
-	where description = 'db-alert-shipconf-24h-fail'
+	where description = 'db-alert-matm-spo-import'
 ) t 
 where (payload::jsonb IS NOT NULL);
