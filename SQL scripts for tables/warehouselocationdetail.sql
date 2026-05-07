383004987487f9a6bdd091c01f3589f68d262e07
atlwkok <weijin.kok@rndc-usa.com>
2026-05-07 11:26:00 -0400

    item-vol-large: pgadmin

--- SQL scripts for tables/warehouselocationdetail.sql
+++ SQL scripts for tables/warehouselocationdetail.sql
@@ -8,14 +8,14 @@ where description like '%tmp-inv-no-resv%';
 -- 	END IF;
 
 -- insert enabled alerts without payload
---  db-alert-open-order-1w.
+--  db-alert-item-vol-large.
 
 
 insert into app.warehouselocationdetail(description, longdescription, warehouseid, requesttypeid, createdby, lastupdatedby) 
     select rt.description, rt.description, w.warehouseid,  rt.requesttypeid, 'sysuser', 'sysuser' 
         from app.warehouselocation w, app.requesttype rt 
         where w.warehouseshortname not in ('DEFAULTS', 'PRODEV') 
-        and rt.description = 'db-alert-open-order-1w' 
+        and rt.description = 'db-alert-item-vol-large' 
         and not exists (select 1 from app.warehouselocationdetail r 
                             where r.requesttypeid = rt.requesttypeid 
                             and r.warehouseid = w.warehouseid);
@@ -35,7 +35,7 @@ from app.warehouselocationdetail r, app.warehouselocation
 
 select *
 	from app.warehouselocationdetail wld, app.warehouselocation wl
-	where wld.description like ('%db-alert-open-order-1w')
+	where wld.description like ('%db-alert-item-vol-large')
 	AND wld.warehouseid = wl.warehouseid 
 	order by wl.warehouseshortname
 
@@ -47,10 +47,10 @@ select *
 -- insert payload for enabled alerts:
 
 
---  Named queries (enabled (ABQ, ARK, AUB, PEN, POR, ROM, WSD) as of writing)
---   ABQ
+--  Named queries (only (ALL) as of writing)
+--   ALL
 update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
+    "alertName":"MAWM - Items Volume With Large Dimensions",
     "dataSource":"MAWM",
     "database":{
         "connection":"MAPRD_NEW",
@@ -61,151 +61,13 @@ update app.warehouselocationdetail set payload = '{
         "subject":"Splunk Alert: $name$",
         "body":"",
         "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"wmsinternal@rndc-usa.com, wmsmspsupport@rndc-usa.com, Ryan.McHenry@rndc-usa.com, FRANCO.FLORES@RNDC-USA.COM",
+        "toEmail":"wmsinternal@RNDC-USA.COM, anamburi@manh.com, DL_RNDC_MAWM_CSO@manh.com, mcurtis@manh.com, WMSMSPSupport@RNDC-USA.COM, DL_MANH_RNDC_MAWM_PSO@manh.com",
         "ccEmail":"",
         "footer":"",
         "attachment":["link-to-alert","link-to-results", "inline-table"]
     },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='ABQ') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
---   ARK
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"Kim.Crawford@rndc-usa.com, Joe.Thompson@rndc-usa.com, Joshua.Edens@rndc-usa.com",
-        "ccEmail":"WMSInternal@rndc-usa.com, WMSMSPSupport@rndc-usa.com, WMSConsultant@rndc-usa.com",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='ARK') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
-
-
---   AUB
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"AUB-Automation-Support@RNDC-USA.COM",
-        "ccEmail":"",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='AUB') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
---   PEN
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"Ryan.Metz@rndc-usa.com, John.Hall@rndc-usa.com, Anthony.Coleman@rndc-usa.com, Dazzle.Keyser@rndc-usa.com, Douglas.Garner@rndc-usa.com",
-        "ccEmail":"wmsinternal@rndc-usa.com, wmsconsultant@rndc-usa.com, WMSMSPSupport@RNDC-USA.COM",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='PEN') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
-
-
-
---   POR
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"bryce.meyer@rndc-usa.com, Michael.Ferdinand@Penske.com, abel.paucardelacruz@rndc-usa.com, Sara.Farrington@Penske.com, Matthew.Chavez@Penske.com,Jesse.Butler@Penske.com, Brian.Bontemps@penske.com",
-        "ccEmail":"",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='POR') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
-
---   ROM
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"Augustus.Narro@RNDC-USA.com, Christopher.Swidergal@RNDC-USA.com, Donald.Boeger@RNDC-USA.com, David.Cedeno@RNDC-USA.com, Carl.Wofford@RNDC-USA.com",
-        "ccEmail":"wmsinternal@rndc-usa.com, wmsconsultant@rndc-usa.com, WMSMSPSupport@RNDC-USA.COM",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='ROM') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
-
-
---   WSD
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"demetrius.dever@rndc-usa.com, Michael.Clark@RNDC-USA.com, guy.sriwatcharakul@rndc-usa.com, hank.dryden@rndc-usa.com",
-        "ccEmail":"wmsinternal@rndc-usa.com",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' where warehouseid = (select w.warehouseid from app.warehouselocation w where w.warehouseshortname='WSD') and requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-open-order-1w');
+    "sql":"SELECT ORG, COUNT(*) AS ''ORG_COUNT'', JSON_ARRAYAGG( JSON_OBJECT( ''ItemId'', ITEM_ID, ''StandardQuantityUomIdDisplay'', STANDARD_QUANTITY_UOM_ID, ''UomId'', UOM_ID, ''Quantity'', QUANTITY ) ) AS ''SPLUNK'' FROM ( SELECT LEFT(II.PROFILE_ID, 3) AS ''ORG'', II.ITEM_ID, II.DESCRIPTION, IP.VOLUME, IP.WEIGHT, IP.QUANTITY, IP.STANDARD_QUANTITY_UOM_ID, IP.UOM_ID FROM default_item_master.ITE_ITEM_PACKAGE IP JOIN default_item_master.ITE_ITEM II ON IP.ITEM_PK = II.PK WHERE IP.STANDARD_QUANTITY_UOM_ID IN (''PACK'', ''UNIT'') AND IP.VOLUME > 100000 AND CONCAT(LEFT(II.PROFILE_ID,3), II.ITEM_ID) IN (SELECT CONCAT(ORG_ID, ITEM_ID) FROM default_dcinventory.DCI_INVENTORY) ) package GROUP BY ORG ORDER BY ORG; "
+}' where requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-item-vol-large');
 
 
 
@@ -215,31 +77,6 @@ update app.warehouselocationdetail set payload = '{
 
 -- (Unnamed)
 --  Disabled queries
-update app.warehouselocationdetail set payload = '{
-    "alertName":"Open Order In Wm > 1 Week",
-    "dataSource":"MAWM",
-    "database":{
-        "connection":"MAPRD_NEW",
-        "type":"MySQL",
-        "uri":"jdbc:{host}:{port}/information_schema"
-    },
-    "email":{
-        "subject":"Splunk Alert: $name$",
-        "body":"",
-        "fromEmail":"wms-tools-noreply@rndc-usa.com",
-        "toEmail":"wmsinternal@RNDC-USA.COM, WMSMSPSupport@RNDC-USA.COM, DL_MANH_RNDC_MAWM_PSO@manh.com, DL_RNDC_MAWM_CSO@manh.com",
-        "ccEmail":"",
-        "footer":"",
-        "attachment":["link-to-alert","link-to-results", "inline-table"]
-    },
-    "sql":"SELECT O.ORG_ID, O.ORIGINAL_ORDER_ID, O.CREATED_TIMESTAMP, O.ORDER_TYPE, CASE WHEN UPPER(ORDER_TYPE) = ''STANDARD'' THEN O.EXT_ROUTE_ID else NULL END AS ''ROUTE'', CONCAT(O.MAXIMUM_STATUS,''-'',DS.DESCRIPTION) ''MAX_STATUS'' from default_dcorder.DCO_ORIGINAL_ORDER O JOIN default_dcorder.DCO_ORDER_STATUS DS ON DS.ORDER_STATUS_ID = O.MAXIMUM_STATUS JOIN default_dcorder.DCO_ORDER_STATUS DS2 ON DS2.ORDER_STATUS_ID = O.MINIMUM_STATUS WHERE 1=1 AND O.MAXIMUM_STATUS < 8000 AND O.CREATED_TIMESTAMP < NOW() - INTERVAL 7 DAY AND O.ORG_ID = $orgId;"
-}' 
-where warehouseid in (select wl.warehouseid
-            from app.warehouselocation wl  
-            where wl.warehouseshortname not in  ('ABQ', 'ARK', 'AUB', 'PEN', 'POR', 'ROM', 'WSD'))
-    and requesttypeid=(select rt.requesttypeid
-                from app.requesttype rt 
-                where rt.description='db-alert-open-order-1w');
 
 
 
@@ -251,7 +88,7 @@ where warehouseid in (select wl.warehouseid
 select wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description != 'db-alert-open-order-1w'
+where description != 'db-alert-item-vol-large'
 and wld.payload is not null
 -- order by wld.payload
 
@@ -261,8 +98,8 @@ and wld.payload is not null
 select  wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description = 'db-alert-open-order-1w'
-and wld.payload is null
+where description = 'db-alert-item-vol-large'
+and wld.payload is not null
 order by wld.lastupdatedon
 
 
@@ -270,7 +107,7 @@ order by wld.lastupdatedon
 select  wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description = 'db-alert-open-order-1w'
+where description = 'db-alert-item-vol-large'
 and wld.payload is not null
 order by wl.warehouseshortname
 
@@ -283,6 +120,6 @@ FROM (
         wd.*, 
         row_number() OVER () AS rn
     FROM app.warehouselocationdetail wd
-	where description = 'db-alert-open-order-1w'
+	where description = 'db-alert-item-vol-large'
 ) t 
 where (payload::jsonb IS NOT NULL);
