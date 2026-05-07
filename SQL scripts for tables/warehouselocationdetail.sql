9646d67922ede0c0c109081f660944371d252d5e
atlwkok <weijin.kok@rndc-usa.com>
2026-05-07 17:13:26 -0400

    item-wt-large: pgadmin

--- SQL scripts for tables/warehouselocationdetail.sql
+++ SQL scripts for tables/warehouselocationdetail.sql
@@ -8,14 +8,14 @@ where description like '%tmp-inv-no-resv%';
 -- 	END IF;
 
 -- insert enabled alerts without payload
---  db-alert-item-vol-large.
+--  db-alert-item-wt-large.
 
 
 insert into app.warehouselocationdetail(description, longdescription, warehouseid, requesttypeid, createdby, lastupdatedby) 
     select rt.description, rt.description, w.warehouseid,  rt.requesttypeid, 'sysuser', 'sysuser' 
         from app.warehouselocation w, app.requesttype rt 
         where w.warehouseshortname not in ('DEFAULTS', 'PRODEV') 
-        and rt.description = 'db-alert-item-vol-large' 
+        and rt.description = 'db-alert-item-wt-large' 
         and not exists (select 1 from app.warehouselocationdetail r 
                             where r.requesttypeid = rt.requesttypeid 
                             and r.warehouseid = w.warehouseid);
@@ -35,7 +35,7 @@ from app.warehouselocationdetail r, app.warehouselocation
 
 select *
 	from app.warehouselocationdetail wld, app.warehouselocation wl
-	where wld.description like ('%db-alert-item-vol-large')
+	where wld.description like ('%db-alert-item-wt-large')
 	AND wld.warehouseid = wl.warehouseid 
 	order by wl.warehouseshortname
 
@@ -46,11 +46,12 @@ select *
 
 -- insert payload for enabled alerts:
 
-
+--  db-alert-item-wt-large.
+ 
 --  Named queries (only (ALL) as of writing)
 --   ALL
 update app.warehouselocationdetail set payload = '{
-    "alertName":"MAWM - Items Volume With Large Dimensions",
+    "alertName":"MAWM - Items Weight With Large Dimensions",
     "dataSource":"MAWM",
     "database":{
         "connection":"MAPRD_NEW",
@@ -66,8 +67,8 @@ update app.warehouselocationdetail set payload = '{
         "footer":"",
         "attachment":["link-to-alert","link-to-results", "inline-table"]
     },
-    "sql":"SELECT ORG, COUNT(*) AS ''ORG_COUNT'', JSON_ARRAYAGG( JSON_OBJECT( ''ItemId'', ITEM_ID, ''StandardQuantityUomIdDisplay'', STANDARD_QUANTITY_UOM_ID, ''UomId'', UOM_ID, ''Quantity'', QUANTITY ) ) AS ''SPLUNK'' FROM ( SELECT LEFT(II.PROFILE_ID, 3) AS ''ORG'', II.ITEM_ID, II.DESCRIPTION, IP.VOLUME, IP.WEIGHT, IP.QUANTITY, IP.STANDARD_QUANTITY_UOM_ID, IP.UOM_ID FROM default_item_master.ITE_ITEM_PACKAGE IP JOIN default_item_master.ITE_ITEM II ON IP.ITEM_PK = II.PK WHERE IP.STANDARD_QUANTITY_UOM_ID IN (''PACK'', ''UNIT'') AND IP.VOLUME > 100000 AND CONCAT(LEFT(II.PROFILE_ID,3), II.ITEM_ID) IN (SELECT CONCAT(ORG_ID, ITEM_ID) FROM default_dcinventory.DCI_INVENTORY) ) package GROUP BY ORG ORDER BY ORG; "
-}' where requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-item-vol-large');
+    "sql":"SELECT ORG, COUNT(*) AS ''ORG_COUNT'', JSON_ARRAYAGG( JSON_OBJECT( ''ItemId'', ITEM_ID, ''StandardQuantityUomIdDisplay'', STANDARD_QUANTITY_UOM_ID, ''UomId'', UOM_ID, ''Quantity'', QUANTITY ) ) AS ''SPLUNK'' FROM ( SELECT LEFT(II.PROFILE_ID, 3) AS ''ORG'', II.ITEM_ID, II.DESCRIPTION, IP.VOLUME, IP.WEIGHT, IP.QUANTITY, IP.STANDARD_QUANTITY_UOM_ID, IP.UOM_ID FROM default_item_master.ITE_ITEM_PACKAGE IP JOIN default_item_master.ITE_ITEM II ON IP.ITEM_PK = II.PK WHERE IP.STANDARD_QUANTITY_UOM_ID IN (''PACK'', ''UNIT'') AND IP.WEIGHT > ''100000'' AND CONCAT(LEFT(II.PROFILE_ID,3), II.ITEM_ID) IN (SELECT CONCAT(ORG_ID, ITEM_ID) FROM default_dcinventory.DCI_INVENTORY) ) package GROUP BY ORG ORDER BY ORG;"
+}' where requesttypeid=(select rt.requesttypeid from app.requesttype rt where rt.description='db-alert-item-wt-large');
 
 
 
@@ -88,7 +89,7 @@ update app.warehouselocationdetail set payload = '{
 select wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description != 'db-alert-item-vol-large'
+where description != 'db-alert-item-wt-large'
 and wld.payload is not null
 -- order by wld.payload
 
@@ -98,7 +99,7 @@ and wld.payload is not null
 select  wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description = 'db-alert-item-vol-large'
+where description = 'db-alert-item-wt-large'
 and wld.payload is not null
 order by wld.lastupdatedon
 
@@ -107,7 +108,7 @@ order by wld.lastupdatedon
 select  wld.payload, wl.warehouseshortname, wld.description
 from app.warehouselocationdetail wld join app.warehouselocation wl
 on wld.warehouseid = wl.warehouseid 
-where description = 'db-alert-item-vol-large'
+where description = 'db-alert-item-wt-large'
 and wld.payload is not null
 order by wl.warehouseshortname
 
@@ -120,6 +121,6 @@ FROM (
         wd.*, 
         row_number() OVER () AS rn
     FROM app.warehouselocationdetail wd
-	where description = 'db-alert-item-vol-large'
+	where description = 'db-alert-item-wt-large'
 ) t 
 where (payload::jsonb IS NOT NULL);
